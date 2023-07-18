965556f17f95ce3dd108d5c649027fb4d6eeed61
bugradostt <bugradostt@gmail.com>
2023-07-18 21:13:58 +0300

    first commit

---
+++ Casgem.ApıLayer/Controllers/CategoryController.cs
@@ -0,0 +1,55 @@
+﻿using Casgem.BusinessLayer.Abstract;
+using Casgem.EntityLayer.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Casgem.ApıLayer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        readonly ICategoryService _categoryService;
+        public CategoryController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+        [HttpGet]
+        public IActionResult ListCategory()
+        {
+            var values = _categoryService.TGetList();
+            return Ok(values);
+        }
+
+        [HttpPost]
+        public IActionResult AddCategory(Category p)
+        {
+            _categoryService.TInsert(p);
+            return Ok("Kategori eklendi");
+        }
+
+        [HttpDelete]
+        public IActionResult DeleteCategory(int id)
+        {
+            var value = _categoryService.TGetById(id);
+            _categoryService.TDelete(value);
+            return Ok();
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetCategory(int id) 
+        {
+            var value = _categoryService.TGetById(id);
+            return Ok(value);
+
+        }
+
+        [HttpPut]
+        public IActionResult PutCategory(Category p)
+        {
+            _categoryService.TUpdate(p);
+            return Ok();
+        }
+    }
+}
---
+++ Casgem.BusinessLayer/Concrete/CustomerManager.cs
@@ -0,0 +1,45 @@
+﻿using Casgem.BusinessLayer.Abstract;
+using Casgem.DataAccessLayer.Abstract;
+using Casgem.EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Casgem.BusinessLayer.Concrete
+{
+    public class CustomerManager : ICustomerService
+    {
+        readonly ICustomerDal  _customerDal;
+
+        public CustomerManager(ICustomerDal customerDal)
+        {
+            _customerDal = customerDal;
+        }
+        public void TDelete(Customer t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Customer TGetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Customer> TGetList()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TInsert(Customer t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TUpdate(Customer t)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
